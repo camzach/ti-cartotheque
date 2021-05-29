171fac4c26514f4d2fe16ac01368e58a1c5b0a2d
Cameron Zach <czach@sevone.com>
2021-05-29 14:14:43 -0400

    add TTS string display

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -9,27 +9,68 @@ const Wrapper = styled.div`
   height: 100%;
 `;
 const Content = styled.div`
-margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const CopyButton = styled.label`
+  content: 'Copy';
+  cursor: pointer;
+  position: relative;
+  background-color: var(--primary);
+  border: 2px solid var(--primary-light);
+  padding: 0.25em;
+  &:active {
+    background-color: var(--primary-light);
+    border: 2px solid var(--primary);
+  }
+  ::before {
+    display: block;
+    position: absolute;
+    top: -1.5em;
+    height: 1.5em;
+    background-color: var(--secondary);
+    content: 'Copied!';
+    opacity: 0;
+    transition: opacity .5s ease .5s;
+  }
+  :active::before {
+    opacity: 1;
+    transition: opacity 0s ease 0s;
+  }
 `;
 
 export default function App() {
-  const [ mapString, setMapString ] = React.useState<number[]>([]);
+  const [ mapString, setMapString ] = React.useState<number[] | null>(null);
   const [ maps, setMaps ] = React.useState<Array<{ name: string, mapString: number[], playerCount: number, requiresPoK: boolean }>>([]);
   const [ loadingMaps, setLoadingMaps ] = React.useState(true);
 
   React.useEffect(() => {(async () => {
-    const res = await (await fetch(`${window.location.origin}/maps`)).json()
-    setMaps(res);
+    const res = await fetch(`${window.location.origin}/maps`);
+    const json = await res.json();
+    setMaps(json);
     setLoadingMaps(false);
   })()}, []);
 
-  console.log(maps);
   return (
   <Wrapper>
     <Sidebar maps={maps} setMapString={setMapString} loadingMaps={loadingMaps} />
     <Content>
-      {/* <input type="text" value={mapString.join} onChange={(e) => setMapString(e.target.value)} style={{outline: '1px solid black'}}/> */}
-      <Map mapString={mapString} />
+      {mapString && <>
+        <Map mapString={mapString} />
+        <div style={{ margin: '2em' }}>
+          <label style={{ color: 'var(--primary-light)' }}>TTS String: </label>
+          <input value={mapString.join(' ')} readOnly />
+          <CopyButton
+            onClick={() => {
+              navigator.clipboard.writeText(mapString.join(' '));
+            }}
+          >
+            Copy to clipboard
+          </CopyButton>
+        </div>
+      </>}
     </Content>
   </Wrapper>);
 }
