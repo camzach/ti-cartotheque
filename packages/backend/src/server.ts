af5d31b1dd695d88262b8e9af5b9de8fa8fbd04e
Cameron Zach <czach@sevone.com>
2021-05-29 14:14:25 -0400

    print mongodb connection error

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -20,7 +20,7 @@ app.get('/', (req, res) => res.sendFile(indexHtml));
 const MONGO_URI = process.env.MONGO_URI || '';
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('connected to mongodb'))
-  .catch(() => console.log('mongodb failed to connect'));
+  .catch((err) => console.log('mongodb failed to connect', err));
 
 app.get('/maps', async (req, res) => {
   try {
