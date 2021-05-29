6271f6ae3e2a9ea6f95d458b420af5624ccdc8f6
Cameron Zach <czach@sevone.com>
2021-05-29 00:44:15 -0400

    get port from env vars

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 const buildPath = path.resolve(__dirname, '../../frontend/dist');
 const indexHtml = path.join(buildPath, 'index.html');
@@ -17,7 +17,7 @@ app.use(express.static(buildPath));
 // Serve index.html on unmatched routes
 app.get('/', (req, res) => res.sendFile(indexHtml));
 
-const { MONGO_URI='' } = process.env;
+const MONGO_URI = process.env.MONGO_URI || '';
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('connected to mongodb'))
   .catch(() => console.log('mongodb failed to connect'));
